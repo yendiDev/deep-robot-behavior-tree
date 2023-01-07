f9dbe4867dc8fd5b2a73d8b969ad74af52262873
yendiDev <kceequan01@gmail.com>
2023-01-07 22:12:41 +0000

    added port communication

---
+++ build/CMakeFiles/grasp_ball_robot_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/grasp_ball_robot_demo.dir/bt_demo.cpp.o"
+  "grasp_ball_robot_demo"
+  "grasp_ball_robot_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/grasp_ball_robot_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
